03991982462e66e0533e3da59916de2d5c88fb31
hungpv <phamvanhung0412@gmail.com>
2025-07-17 09:06:29 +0700

    fix:add cors

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(builder =>
     {
-        builder.WithOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:5173")
+        builder.WithOrigins("http://localhost:3000", "http://localhost:3001", "http://localhost:5173", "https://dashboard-email-z9nf.vercel.app")
                .AllowAnyHeader()
                .AllowAnyMethod();
     });
